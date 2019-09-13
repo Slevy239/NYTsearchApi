fc0c63e811180c6c84e42467152214243bf338b8
Phil Swartz <pswartz.sju@gmail.com>
2019-09-12 21:41:46 -0400

    app.js now displays the url

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,8 +3,9 @@
 // APIKEY from the new york search developer app
 var APIKEY = "9pCVN4K5GzuFggvtfVXmxsiCGCOOsuwC";
 
+var articles = $('#top-articles').html();
 
-
+var myURL;
 
 // on click function to begin a search for when the user clicks search button
 $('#search').on('click', function(event){
@@ -13,8 +14,8 @@ $('#search').on('click', function(event){
 
     // create a variable to store the users search string
     var searchTerm = $('#searchTerm').text();
-    
 
+    /* var articles = $('#top-articles').html(); */
 
     var searchTerm = 'cheeseburgers';
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+'&api-key='+APIKEY;
@@ -25,14 +26,22 @@ $('#search').on('click', function(event){
         method: "GET"
     }).then(function(response){
     
-        
-
         console.log(response);
+
+        myURL = response.response.docs[0].web_url;
+        //console.log(myURL[0]);
+        
     
-    
+        console.log(myURL);
+
     })
 
 
+    $('#top-articles').append('<div>');
+    $('#top-articles > div').text(myURL);
+
+    
+    
 
 
 })
